31094af5566349d61709cdde6113afbfab907153
amiinegal <you@example.com>
2019-03-31 15:42:47 +0300

    add new file

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import{FormControl} from '@angular/forms';
+import{GithubService} from '../../services/github.service';
+import {EMPTY, from} from 'rxjs';
+import {User} from '../../models/user.model';
+import {filter, switchMap, debounceTime, catchError} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private githubService: GithubService) { }
+findControl = new FormControl();
+error: boolean = false;
+user:User = null;
+
+  ngOnInit() {
+    this.findControl.valueChanges
+    .pipe(filter(value=> value.length > 3),
+    debounceTime(1000),
+    switchMap(value => this.githubService.getUser(value).pipe(catchError(err => {
+      this.user = null;
+      this.error = true;
+      return EMPTY;
+    })))
+    ).subscribe(user => this.user = user );
+  }
+
+}
