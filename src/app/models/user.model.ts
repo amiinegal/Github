03e0e1b292000a7bd324b6f43a6dd70dee268054
amiinegal <you@example.com>
2019-03-31 15:43:46 +0300

    add new changes

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    login: string;
+    avatar_url: string;
+    created_at: string;
+    location: string;
+    bio: string;
+    public_repos: string;
+    public_gists: string;
+    html_url:string;
+    followers:string;
+    following:string;
+    name:string;
+}
